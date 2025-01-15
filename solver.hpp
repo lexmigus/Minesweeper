41e450e5ab5c74782533e29cf4e595903882ba25
lexmigus <alexanderpmgs@gmail.com>
2025-01-15 13:16:02 -0800

    Trivial Solver Implemented

--- solver.hpp
+++ solver.hpp
@@ -1,32 +1,37 @@
 #include "board.hpp"
 
-//solve runs the high level solving logic
-void solve(Board& board) {
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            continue;
-        }
-    }
-}
-
 //solveCell runs the solving logic for a single cell
 void solveCell(Board& board, std::pair<int, int> cell) {
     int x = cell.first;
     int y = cell.second;
-    if (board.allNeighboursKnown(x, y) or board.allNeighboursUnknown(x, y)) {
-        return;
-    }
-    if (board.isHidden(x, y)) {
+    if (board.allNeighboursKnown(x, y) or board.isHidden(x, y)) {
         return;
     }
     else {
         int surroundingFlags = board.countSurroundingFlags(x, y);
         int surroundingHidden = board.countSurroundingHidden(x, y);
-        if (surroundingFlags == board.checkSurroundingCellCounts(x, y)) {
+        int numberOfMines = board.checkSurroundingCellCounts(x, y);
+
+        if (surroundingFlags == numberOfMines) {
+            std::cout << "revealing" << std::endl;
             board.revealAllNeighbors(x, y);
         }
-        else if (surroundingFlags + surroundingHidden == board.checkSurroundingCellCounts(x, y)) {
+        else if (surroundingFlags + surroundingHidden == numberOfMines) {
+            std::cout << "flagging" << std::endl;
             board.flagAllNeighbors(x, y);
         }
+    }
+}
+
+
+//solve runs the high level solving logic
+void solve(Board& board) {
+    for (int i=0; i<5; i++){
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                solveCell(board, {i, j});
+            }
+        }
+        board.printBoard();
     }
 }
